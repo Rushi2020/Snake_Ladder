f415992e8363d9919bb9efe4a4015888ae06bda1
Rushi <189489rishi@gmail.com>
2022-02-13 22:03:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Player start with position 0
+
+using System;
+
+namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("snake ladder game");
+            GamePlay();
+        }
+
+        public static void GamePlay()
+        {
+            int position = 0;
+        }
+    }
+}
