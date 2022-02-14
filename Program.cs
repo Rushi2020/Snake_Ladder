d67691eed5d502f83043b3de211534b12fe8082e
Rushi <189489rishi@gmail.com>
2022-02-14 11:53:38 +0530

    Added:Repeat till the player reaches 100 and restrict position value to get below 0

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// Player start with position 0
-
+﻿// See https://aka.ms/new-console-template for more information
 using System;
 
 namespace SnakeLadder
@@ -14,7 +13,50 @@ namespace SnakeLadder
 
         public static void GamePlay()
         {
-            int position = 0;
+            //constants
+            const int LADDER = 1;
+            const int SNAKE = 2;
+            const int NO_PLAY = 3;
+
+            DiceRolling diceRolling = new DiceRolling();
+            UserInfo user = new UserInfo();
+
+
+            while (user.UserPosition < 100)
+            {
+                //variables
+                int diceNumber = diceRolling.DiceRoll();
+                int functionNumber = diceRolling.Function();
+                Console.WriteLine(diceNumber);
+                switch (functionNumber)
+                {
+                    case LADDER:
+                        //user.UserPosition= user.UserPosition + diceNumber <= 100? user.UserPosition: user.UserPosition + diceNumber;
+                        if (user.UserPosition + diceNumber <= 100)
+                        {
+                            user.UserPosition += diceNumber;
+                        }
+                        break;
+                    case SNAKE:
+                        if (user.UserPosition > diceNumber)
+                        {
+                            user.UserPosition -= diceNumber;
+                        }
+                        else
+                        {
+                            user.UserPosition = 0;
+                            Console.WriteLine("user position negative");
+                        }
+                        break;
+                    case NO_PLAY:
+                        Console.WriteLine("no play");
+                        break;
+                    default:
+                        Console.WriteLine("default case error");
+                        break;
+                }
+                Console.WriteLine(user.UserPosition);
+            }
         }
     }
 }
